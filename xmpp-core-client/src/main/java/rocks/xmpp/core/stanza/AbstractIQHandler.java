c4817b511cc42e7a1a5593e0ae0fa85582673ac4
Christian Schudt <christian.schudt@gmx.de>
2015-02-22 18:29:53 +0100

    Include a reason, when returning a <bad-request/> error during IQ handling.

--- xmpp-core-client/src/main/java/rocks/xmpp/core/stanza/AbstractIQHandler.java
+++ xmpp-core-client/src/main/java/rocks/xmpp/core/stanza/AbstractIQHandler.java
@@ -26,6 +26,7 @@ package rocks.xmpp.core.stanza;
 
 import rocks.xmpp.core.session.Manager;
 import rocks.xmpp.core.stanza.model.AbstractIQ;
+import rocks.xmpp.core.stanza.model.StanzaError;
 import rocks.xmpp.core.stanza.model.client.IQ;
 import rocks.xmpp.core.stanza.model.errors.Condition;
 
@@ -62,7 +63,7 @@ public abstract class AbstractIQHandler implements IQHandler {
             if (iq.getType() == type) {
                 return processRequest(iq);
             } else {
-                return iq.createError(Condition.BAD_REQUEST);
+                return iq.createError(new StanzaError(Condition.BAD_REQUEST, String.format("Type was '%s', but expected '%s'.", iq.getType().toString().toLowerCase(), type.toString().toLowerCase())));
             }
         }
         return iq.createError(Condition.SERVICE_UNAVAILABLE);

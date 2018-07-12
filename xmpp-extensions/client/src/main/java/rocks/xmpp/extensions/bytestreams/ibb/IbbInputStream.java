sco0ter/babbler

commit bfd184fa09a15f8b608516f084a8ef4603cc68ca

Fix synchronization issues in IBB logic

xmpp-extensions/client/src/main/java/rocks/xmpp/extensions/bytestreams/ibb/IbbInputStream.java
@@ -105,26 +105,23 @@ final class IbbInputStream extends InputStream {
                 return false;
             }
             timeout = readTimeout;
-        }
-        // If the buffer is empty, retrieve the next data packet and load it into the buffer.
-        if (n == -1 || n >= buffer.length) {
-            try {
-                InBandByteStream.Data data = null;
-                if (timeout <= 0) {
-                    while (data == null || data.getSequence() == -1) {
-                        synchronized (this) {
+
+            // If the buffer is empty, retrieve the next data packet and load it into the buffer.
+            if (n == -1 || n >= buffer.length) {
+                try {
+                    InBandByteStream.Data data = null;
+                    if (timeout <= 0) {
+                        while (data == null || data.getSequence() == -1) {
                             // If the stream has been closed and there's no more data to process, return -1.
                             if (closed && queue.isEmpty()) {
                                 return false;
                             }
+                            // Let's see, if there's some data for me.
+                            data = queue.poll(1, TimeUnit.SECONDS);
                         }
-                        // Let's see, if there's some data for me.
-                        data = queue.poll(1, TimeUnit.SECONDS);
-                    }
-                } else {
-                    data = queue.poll(timeout, TimeUnit.MILLISECONDS);
-                    if (data == null || data.getSequence() == -1) {
-                        synchronized (this) {
+                    } else {
+                        data = queue.poll(timeout, TimeUnit.MILLISECONDS);
+                        if (data == null || data.getSequence() == -1) {
                             if (closed && queue.isEmpty()) {
                                 return false;
                             } else {
@@ -132,17 +129,17 @@ final class IbbInputStream extends InputStream {
                             }
                         }
                     }
-                }
-                synchronized (this) {
+
                     // Assign the new buffer.
                     buffer = data.getBytes();
                     n = 0;
+
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    InterruptedIOException ie = new InterruptedIOException();
+                    ie.initCause(e);
+                    throw ie;
                 }
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                InterruptedIOException ie = new InterruptedIOException();
-                ie.initCause(e);
-                throw ie;
             }
         }
         return true;

xmpp-extensions/client/src/main/java/rocks/xmpp/extensions/bytestreams/ibb/IbbSession.java
@@ -84,12 +84,7 @@ final class IbbSession extends ByteStreamSession {
     }
 
     final synchronized boolean dataReceived(InBandByteStream.Data data) {
-        if (inboundSequence++ == data.getSequence()) {
-            inputStream.queue.offer(data);
-            return true;
-        } else {
-            return false;
-        }
+        return inboundSequence++ == data.getSequence() && inputStream.queue.offer(data);
     }
 
     final AsyncResult<IQ> open() {

commit df4860a429f209d8e62480c3aa6b0f694ae3fb3c

Resolve deadlock in IbbInputStream
Introduced with bfd184f

xmpp-extensions/client/src/main/java/rocks/xmpp/extensions/bytestreams/ibb/IbbInputStream.java
@@ -100,28 +100,33 @@ final class IbbInputStream extends InputStream {
     private boolean fillBuffer() throws IOException {
 
         long timeout;
+        boolean bufferEmpty;
         synchronized (this) {
             if (closed && queue.isEmpty()) {
                 return false;
             }
             timeout = readTimeout;
-
-            // If the buffer is empty, retrieve the next data packet and load it into the buffer.
-            if (n == -1 || n >= buffer.length) {
-                try {
-                    InBandByteStream.Data data = null;
-                    if (timeout <= 0) {
-                        while (data == null || data.getSequence() == -1) {
+            bufferEmpty = n == -1 || n >= buffer.length;
+        }
+        // If the buffer is empty, retrieve the next data packet and load it into the buffer.
+        if (bufferEmpty) {
+            try {
+                InBandByteStream.Data data = null;
+                if (timeout <= 0) {
+                    while (data == null || data.getSequence() == -1) {
+                        synchronized (this) {
                             // If the stream has been closed and there's no more data to process, return -1.
                             if (closed && queue.isEmpty()) {
                                 return false;
                             }
-                            // Let's see, if there's some data for me.
-                            data = queue.poll(1, TimeUnit.SECONDS);
                         }
-                    } else {
-                        data = queue.poll(timeout, TimeUnit.MILLISECONDS);
-                        if (data == null || data.getSequence() == -1) {
+                        // Let's see, if there's some data for me.
+                        data = queue.poll(1, TimeUnit.SECONDS);
+                    }
+                } else {
+                    data = queue.poll(timeout, TimeUnit.MILLISECONDS);
+                    if (data == null || data.getSequence() == -1) {
+                        synchronized (this) {
                             if (closed && queue.isEmpty()) {
                                 return false;
                             } else {
@@ -129,17 +134,17 @@ final class IbbInputStream extends InputStream {
                             }
                         }
                     }
-
+                }
+                synchronized (this) {
                     // Assign the new buffer.
                     buffer = data.getBytes();
                     n = 0;
-
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    InterruptedIOException ie = new InterruptedIOException();
-                    ie.initCause(e);
-                    throw ie;
                 }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                InterruptedIOException ie = new InterruptedIOException();
+                ie.initCause(e);
+                throw ie;
             }
         }
         return true;